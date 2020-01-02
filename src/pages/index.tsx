63376aceaa924165158f04773fed751c6ab163d8
clymene <326795631@qq.com>
2020-01-02 20:42:34 +0800

    ++

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Redirect from 'umi/redirect';
 
 export default function() {
+  useEffect(()=>{
+    sessionStorage.setItem('openKeys','001');
+    sessionStorage.setItem('keys','001.001');
+  },[]);
   return <Redirect to={`/but/home`}/>;
 }
 
