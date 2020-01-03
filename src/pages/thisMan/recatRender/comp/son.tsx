3485511217dff66bff1684cb51a010518bd8481e
clymene <326795631@qq.com>
2020-01-03 11:15:41 +0800

    333

--- src/pages/thisMan/recatRender/comp/son.tsx
+++ src/pages/thisMan/recatRender/comp/son.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 export default function index(prop:any) {
-  const {a = ''} = prop;
-  // console.log(a,'render了');
+  const {a = '', children = ''} = prop;
+  const renderA = (a:any) =>{
+    // 使用useMemo避免不必要的render
+    console.log('render a');
+    return `a 改变了 ${a} 次`;
+  };
+  console.log('render son inside');
   return (
-    <div>
-      这个是儿啊
+    <div style={{marginTop:20}}>
+      <h1>
+        这个是儿啊
+      </h1>
+      <div>
+        {
+          useMemo(()=>{
+            return renderA(a)
+          },[a])
+        }
+      </div>
+      <div>
+        {
+          useMemo(()=>{
+            console.log('render children');
+            return (
+              <div>
+                {children}
+              </div>
+            )
+          },[children])
+        }
+      </div>
     </div>
   )
 }
+
--- src/pages/thisMan/recatRender/index.tsx
+++ src/pages/thisMan/recatRender/index.tsx
@@ -1,12 +1,41 @@
-import React, {useState} from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Son from './comp/son';
+import {awsl} from 'src/pages/services/login';
 export default function index() {
   const [count, setCount] = useState<number>(0);
+  const [time, setTime] = useState<any>('init time');
+  useEffect(()=>{
+    // useEffect只能获取到 count 和 time 的变化，并不能拦截
+    console.log(count,time,' useEffect ');
+  },[count,time]);
+  const Child = () => {
+    // 父组件自己刷新，子组件props不变也要刷新:
+    console.log("render other child");
+    return <div>child</div>;
+  };
+  const getAPI = async () => {
+    const {data:{ a = undefined, b = undefined }={}} = await awsl({});
+    setCount(a);
+  };
+
+  // 减少 render 的次数 类组件可以使用 shouldComponentUpdate 或 PureComponent，函数组件可以利用高级组件的特性或者 React.memo
+  // 对组件进行合理的拆分
+
   return (
     <div>
-      父组件自己刷新，子组件props不变也要刷新:{count}
-      <button onClick={()=>{setCount(count + 1)}}>点我</button>
-      <Son a={'1'}/>
+      <button onClick={()=>{setCount(count)}}>点我</button>
+      <button onClick={()=>{setTime(new Date().getTime())}}>time</button>
+      <button onClick={getAPI}>getAPI</button>
+      {
+        useMemo(()=>{
+          // 可以使用在组件外部来控制只有那些参数变化才触发组件的渲染,但是里面不能async来请求
+          console.log('render son out');
+          return (
+            <Son a={count}>{time}</Son>
+          )
+        },[count,time])
+      }
+      <Child/>
     </div>
   )
 }
