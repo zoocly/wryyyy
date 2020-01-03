f6111bb45aff36f48bb1084b4fff86229d9f8ba7
clymene <326795631@qq.com>
2020-01-03 09:42:14 +0800

    删除没成功的live2d

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -2,28 +2,7 @@ import React from 'react';
 import { Layout, Breadcrumb } from 'antd';
 import Menu from 'src/components/Menu';
 import style from './index.css';
-// import L2Dwidget from '../../public/L2Dwidget.min.js'
 const { Header, Content, Footer, Sider } = Layout;
-// function initL2Dwidget() {
-//   console.log(L2Dwidget,'sss')
-//   // L2Dwidget.init({
-//   //     dialog: {
-//   //       // 开启对话框
-//   //       enable: true,
-//   //       script: {
-//   //         // 当触摸到角色身体
-//   //         'tap body': '哎呀！别碰我！',
-//   //         // 当触摸到角色头部
-//   //         'tap face': '人家是在认真写博客哦--前端妹子',
-//   //       }
-//   //     },
-//   //     display: {
-//   //       position: 'right'
-//   //     },
-//   //     "model": { "jsonPath": "live2d-widget-model-z16/assets/z16.model.json" },
-//   //     "mobile": { "show": true, scale: 0.5 },
-//   //   });
-// }
 class BasicLayout extends React.Component<any, any> {
   constructor(props:any){
     super(props);
@@ -31,18 +10,6 @@ class BasicLayout extends React.Component<any, any> {
 
     }
   };
-  componentWillMount(): void {
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.async = true;
-    script.src = '../public/L2Dwidget.min.js';
-    document.head.appendChild(script);
-  }
-
-  componentDidMount(): void {
-    // initL2Dwidget();
-  }
-
   onCollapse=(collapsed:any)=>{
     this.setState({ collapsed });
   };
