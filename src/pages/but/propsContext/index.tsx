8dee6e99468534f910756ad8072f8e01f83ce5ca
clymene <326795631@qq.com>
2020-03-16 09:52:15 +0800

    使用Hooks+resize可以实时获取浏览器宽度

--- src/pages/but/propsContext/index.tsx
+++ src/pages/but/propsContext/index.tsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import {Divider} from 'antd';
 import {Provider} from './config';
 import Son from './comp/son';
 import {awsl} from 'src/pages/services/login'
@@ -9,6 +10,14 @@ export default function index() {
     setObj(res.data);
   };
 
+  const [ width ,setWidth ] = useState(window.innerWidth);
+
+  useEffect(()=>{
+    const getWidth = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", getWidth);
+    return ()=> window.removeEventListener("resize", getWidth);
+  },[]);
+
   return (
     <Fragment>
       context传值<br/>
@@ -17,6 +26,9 @@ export default function index() {
         不晓得有撒子问题
         <Son/>
       </Provider>
+      <Divider/>
+      使用Hooks+resize可以实时获取浏览器宽度<br/>
+      window.innerWidth的值:{width}
     </Fragment>
   )
 }
