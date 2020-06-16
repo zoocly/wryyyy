181020ef235fe9518bd42ce3cdcdb0d839460b35
clymene <326795631@qq.com>
2020-06-16 10:27:04 +0800

    生成uuid方法

--- src/utils/method.js
+++ src/utils/method.js
@@ -26,6 +26,13 @@ export function jsonToTree(data=[], parent='parent', code='code',parentID='-1' )
   return result;
 }
 
+export const uuid=()=> {
+  var temp_url = URL.createObjectURL(new Blob());
+  var uuid = temp_url.toString(); // blob:https://xxx.com/b250d159-e1b6-4a87-9002-885d90033be3
+  URL.revokeObjectURL(temp_url);
+  return uuid.substr(uuid.lastIndexOf("/") + 1);
+};
+
 export const toChineseNum = (num) => {
   if(typeof num !== 'number'){
     return num;
