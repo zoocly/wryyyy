aa4d5c2d3ac9defa8c7e5182ece2cab8b8cc3b3f
clymene <326795631@qq.com>
2020-11-16 21:53:01 +0800

    1

--- src/components/DynamicTableHead/index.tsx
+++ src/components/DynamicTableHead/index.tsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useRef, useState, useMemo, Fragment } from 'react';
+import React, { Fragment, useEffect, useMemo, useRef, useState } from 'react';
 import _isEmpty from 'lodash/isEmpty';
 import _cloneDeep from 'lodash/cloneDeep';
 import _isNumber from 'lodash/isNumber';
 import _get from 'lodash/get';
 import { treeToList } from '@/utils/method.js';
-const fakeLine = (arr:Array<any>) =>{
+
+const fakeLine = (arr: Array<any>) => {
   let num = 0;
-  const addNum = (arr:Array<any>) =>{
-    arr.map(it =>{
-      if(_isEmpty(it.children)){
-        num += 1
-      }else {
-        addNum(it.children)
+  const addNum = (arr: Array<any>) => {
+    arr.map(it => {
+      if (_isEmpty(it.children)) {
+        num += 1;
+      } else {
+        addNum(it.children);
       }
     });
     return num;
   };
   return addNum(arr);
 };
-const Colgroup = (props:any) =>{
+const Colgroup = (props: any) => {
   const { tree, nodeWith = 100 } = props;
   let num = fakeLine(tree);
-  let gird = [ new Array(num).fill('').map(() => { return {value:' '} })];
+  let gird = [new Array(num).fill('').map(() => {
+    return { value: ' ' };
+  })];
   return (
     <Fragment>
       {
@@ -29,55 +32,59 @@ const Colgroup = (props:any) =>{
           return (
             <colgroup>
               {
-                new Array(_get(gird,'[0].length',1)).fill('').map((i, index) => <col key={index} style={{width: nodeWith}}/>)
+                new Array(_get(gird, '[0].length', 1)).fill('').map((i, index) => <col key={index}
+                                                                                       style={{ width: nodeWith }} />)
               }
             </colgroup>
-          )
+          );
         }, [num])
       }
     </Fragment>
 
-  )
+  );
 };
-const Head = (props:any) => {
-  const { tree, nodeName, stepFunc } = props;
+const Head = (props: any) => {
+  const {
+    tree,
+    nodeName,
+    stepFunc,
+  } = props;
   const depthRef = useRef(0);
+  const [team, setTeam] = useState<Array<any>>([]);
 
-  const [ team, setTeam ] = useState<Array<any>>([]);
-
-  const maxDeep = (arr:Array<any>) =>{
+  const maxDeep = (arr: Array<any>) => {
     let max = 0;
-    if(_isEmpty(arr)){
-      return 0
+    if (_isEmpty(arr)) {
+      return 0;
     }
-    arr.map(it=>{
+    arr.map(it => {
       let count = maxDeep(it.children);
-      if(max < count){
-        max = count
+      if (max < count) {
+        max = count;
       }
     });
     return max + 1;
   };
-  const addNum = (arr:Array<any>) =>{
-    arr.map(it =>{
-      if(_isEmpty(it.children)){
+  const addNum = (arr: Array<any>) => {
+    arr.map(it => {
+      if (_isEmpty(it.children)) {
         it.colSpan = 1;
-      }else {
+      } else {
         addNum(it.children);
       }
     });
     return arr;
   };
-  const f = (arr:Array<any>) =>{
-    if(_isEmpty(arr)){
-      return arr
+  const f = (arr: Array<any>) => {
+    if (_isEmpty(arr)) {
+      return arr;
     }
-    arr.map(it =>{
-      if(!_isNumber(it.colSpan) || it.colSpan === 0){  // 没有累加上来的colSpan
-        if(!_isEmpty(it.children)){
+    arr.map(it => {
+      if (!_isNumber(it.colSpan) || it.colSpan === 0) {  // 没有累加上来的colSpan
+        if (!_isEmpty(it.children)) {
           let num = 0;
           f(it.children); // 又没累加上来，还有children的，甩下去遍历
-          it.children.map((item:any) => {
+          it.children.map((item: any) => {
             num += item.colSpan;
           });
           it.colSpan = num;
@@ -86,67 +93,72 @@ const Head = (props:any) => {
     });
     return arr;
   };
-  const fc = (arr:Array<any>) =>{
-    arr.map(it =>{
-      if(!_isEmpty(it.children)){
+  const fc = (arr: Array<any>) => {
+    arr.map(it => {
+      if (!_isEmpty(it.children)) {
         it.rowSpan = 1;
         fc(it.children);
-      }else {
+      } else {
         // it.rowSpan = depthRef.current - it[nodeKey].length/2 + 1 ;
-        it.rowSpan = depthRef.current - stepFunc(it) ;
+        it.rowSpan = depthRef.current - stepFunc(it);
       }
     });
     return arr;
   };
-  const groupBy = (array:Array<any>, name:string) =>{
-    const groups:any = {};
-    array.forEach(function (o) {
+  const groupBy = (array: Array<any>, name: string) => {
+    const groups: any = {};
+    array.forEach(function(o) {
       const group = JSON.stringify(o[name]);
       groups[group] = groups[group] || [];
-      groups[group].push(o)
+      groups[group].push(o);
     });
-    return Object.keys(groups).map(function (group) {
-      return groups[group]
-    })
+    return Object.keys(groups).map(function(group) {
+      return groups[group];
+    });
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     let _data = _cloneDeep(tree);
     // 树深度
     let depth = maxDeep(_data);
     depthRef.current = depth;
     // 加上rowSpan和colSpan
     _data = addNum(_data);
-    for(let i = 0; i < depth - 1; i++){ _data = f(_data) }
+    for (let i = 0; i < depth - 1; i++) {
+      _data = f(_data);
+    }
     _data = fc(_data);
     // 平铺树，按层级分组
-    let teamArr = groupBy(treeToList(_cloneDeep(_data),true).map((it:any)=>{ return { ...it, step:stepFunc(it) } }),'step');
+    let teamArr = groupBy(treeToList(_cloneDeep(_data), true).map((it: any) => {
+      return { ...it, step: stepFunc(it) };
+    }), 'step');
     setTeam(teamArr);
-    return ()=>{
+    return () => {
       setTeam([]);
       depthRef.current = 0;
-    }
-  },[JSON.stringify(tree)]);
+    };
+  }, [JSON.stringify(tree)]);
 
   return (
     <thead>
     {
-      team.map((it:any, index:number)=>{
+      team.map((it: any, index: number) => {
         return (
           <tr key={index}>
             {
-              it.map((item:any, indexx:number) =>{
+              it.map((item: any, indexx: number) => {
                 return (
-                  <td style={{ border:' 1px solid #000', textAlign: 'center'}} key={indexx} colSpan={item.colSpan} rowSpan={item.rowSpan} >{item[nodeName]}</td>
-                )
+                  <td style={{ border: ' 1px solid #000', textAlign: 'center' }} key={indexx} colSpan={item.colSpan}
+                      rowSpan={item.rowSpan}>{item[nodeName]}</td>
+                );
               })
             }
           </tr>
-        )
+        );
       })
     }
     </thead>
-  )
+  );
 };
-export  { Head, Colgroup, fakeLine };
+export { Head, Colgroup, fakeLine };
 
--- src/pages/thisMan/dynamicTatbleHeadText/index.tsx
+++ src/pages/thisMan/dynamicTatbleHeadText/index.tsx
@@ -1,74 +1,75 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react';
-import { Head, Colgroup } from '../../../components/DynamicTableHead';
+import React, { Fragment } from 'react';
+import { Colgroup, Head as SelfHead } from '../../../components/DynamicTableHead';
 import ReactDataSheet from 'react-datasheet';
 import 'react-datasheet/lib/react-datasheet.css';
-const index = (props:any) =>{
+
+const index = (props: any) => {
   const res = {
-    "code": 0,
-    "data": [{
-      "children": [{
-        "k0100": "011A60CE22B44A3FA4CE885703C0898A",
-        "k0500": "061D3B2DEBF44F8096CE12A47D772934",
-        "k0501": "0101",
-        "k0505": "指标名称2",
-        "k0506": "考核内容",
-        "parent":'01',
+    'code': 0,
+    'data': [{
+      'children': [{
+        'k0100': '011A60CE22B44A3FA4CE885703C0898A',
+        'k0500': '061D3B2DEBF44F8096CE12A47D772934',
+        'k0501': '0101',
+        'k0505': '指标名称2',
+        'k0506': '考核内容',
+        'parent': '01',
       }],
-      "k0100": "011A60CE22B44A3FA4CE885703C0898A",
-      "k0500": "24203488BF9846C5B968F9D489D54FB5",
-      "k0501": "01",
-      "k0505": "指标名称1",
-      "k0506": "考核内容",
-      "parent":'-1'
+      'k0100': '011A60CE22B44A3FA4CE885703C0898A',
+      'k0500': '24203488BF9846C5B968F9D489D54FB5',
+      'k0501': '01',
+      'k0505': '指标名称1',
+      'k0506': '考核内容',
+      'parent': '-1',
     }, {
-      "children": [{
-        "k0100": "011A60CE22B44A3FA4CE885703C0898A",
-        "k0500": "DFD59522FAEC4ECCA0C6E987AC74D224",
-        "k0501": "0401",
-        "parent":'04',
-        "k0505": "指标四",
-        "k0506": "考核内容四"
+      'children': [{
+        'k0100': '011A60CE22B44A3FA4CE885703C0898A',
+        'k0500': 'DFD59522FAEC4ECCA0C6E987AC74D224',
+        'k0501': '0401',
+        'parent': '04',
+        'k0505': '指标四',
+        'k0506': '考核内容四',
       }, {
-        "k0100": "011A60CE22B44A3FA4CE885703C0898A",
-        "k0500": "44BFD72785A54E84921F32D8C6DAD8BA",
-        "k0501": "0402",
-        "parent":'04',
-        "k0505": "指标四",
-        "k0506": "考核内容四"
+        'k0100': '011A60CE22B44A3FA4CE885703C0898A',
+        'k0500': '44BFD72785A54E84921F32D8C6DAD8BA',
+        'k0501': '0402',
+        'parent': '04',
+        'k0505': '指标四',
+        'k0506': '考核内容四',
       }],
-      "k0100": "011A60CE22B44A3FA4CE885703C0898A",
-      "k0500": "BB5D8E19B67B48139E7811F32837FAB7",
-      "k0501": "04",
-      "parent":'-1',
-      "k0505": "指标四",
-      "k0506": "考核内容四"
+      'k0100': '011A60CE22B44A3FA4CE885703C0898A',
+      'k0500': 'BB5D8E19B67B48139E7811F32837FAB7',
+      'k0501': '04',
+      'parent': '-1',
+      'k0505': '指标四',
+      'k0506': '考核内容四',
     }, {
-      "children": [{
-        "children": [{
-          "k0501": "020202",
-          "k0505": "指标名称5",
-          "parent":'0202',
-          "children":[{
-            "k0501": "02020202",
-            "k0505": "指标名称52222",
-          }]
-        },],
-        "k0100": "011A60CE22B44A3FA4CE885703C0898A",
-        "k0500": "15B6BD4597AE7147715E17208F975E03",
-        "k0501": "0202",
-        "parent":'02',
-        "k0505": "指标名称7",
-        "k0506": "考核内容"
+      'children': [{
+        'children': [{
+          'k0501': '020202',
+          'k0505': '指标名称5',
+          'parent': '0202',
+          'children': [{
+            'k0501': '02020202',
+            'k0505': '指标名称52222',
+          }],
+        }],
+        'k0100': '011A60CE22B44A3FA4CE885703C0898A',
+        'k0500': '15B6BD4597AE7147715E17208F975E03',
+        'k0501': '0202',
+        'parent': '02',
+        'k0505': '指标名称7',
+        'k0506': '考核内容',
       }],
-      "k0100": "011A60CE22B44A3FA4CE885703C0898A",
-      "k0500": "1508F975E7715E17203B6BD4597AE714",
-      "k0501": "02",
-      "parent":'-1',
-      "k0505": "指标名称5",
-      "k0506": "考核内容"
+      'k0100': '011A60CE22B44A3FA4CE885703C0898A',
+      'k0500': '1508F975E7715E17203B6BD4597AE714',
+      'k0501': '02',
+      'parent': '-1',
+      'k0505': '指标名称5',
+      'k0506': '考核内容',
     }],
-    "message": "操作成功",
-    "version": "dev-pg-3.1.02"
+    'message': '操作成功',
+    'version': 'dev-pg-3.1.02',
   };
   const { data = [] } = res;
 
@@ -76,30 +77,30 @@ const index = (props:any) =>{
     <div>
       <Fragment>
         <table>
-          <Head tree={data} nodeName={'k0505'} stepFunc={ (it:any)=>it.k0501.length/2 - 1 }/>
-          <Colgroup tree={data}/>
+          <SelfHead tree={data} nodeName={'k0505'} stepFunc={(it: any) => it.k0501.length / 2 - 1} />
+          <Colgroup tree={data} />
         </table>
-        <ReactDataSheet
-          style={{width:'100%'}}
-          data={[]}
-          valueRenderer={(cell:any) => cell['value']}
-          sheetRenderer={(props:any) => {
-            return (
-              <table>
-                <Colgroup tree={data}/>
-                <tbody>{props.children}</tbody>
-              </table>
-            );
-          }}
-          cellRenderer={(props:any)=>{
-            const {cell = {}} = props || {};
-            return (
-              <td style={{height:28}}>{cell['value']}</td>
-            )
-          }}
-        />
+        {/*<ReactDataSheet*/}
+        {/*  style={{ width: '100%' }}*/}
+        {/*  data={[]}*/}
+        {/*  valueRenderer={(cell: any) => cell['value']}*/}
+        {/*  sheetRenderer={(props: any) => {*/}
+        {/*    return (*/}
+        {/*      <table>*/}
+        {/*        <Colgroup tree={data} />*/}
+        {/*        <tbody>{props.children}</tbody>*/}
+        {/*      </table>*/}
+        {/*    );*/}
+        {/*  }}*/}
+        {/*  cellRenderer={(props: any) => {*/}
+        {/*    const { cell = {} } = props || {};*/}
+        {/*    return (*/}
+        {/*      <td style={{ height: 28 }}>{cell['value']}</td>*/}
+        {/*    );*/}
+        {/*  }}*/}
+        {/*/>*/}
       </Fragment>
     </div>
-  )
+  );
 };
 export default index;
