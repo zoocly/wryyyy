7eefd5dd3548873a55091ac2171c2914fc1158bd
clymene <326795631@qq.com>
2020-11-26 17:32:29 +0800

    rem适配的方案

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,17 @@ export default function() {
   useEffect(()=>{
     sessionStorage.setItem('openKeys','001');
     sessionStorage.setItem('keys','001.001');
+
+    /*
+    * 一种自适应的方式
+    * 动态设置根fontSize的大小，适配rem
+    * clientWidth / 100 = 1X = 1rem
+    * */
+    // window.onresize = ()=>{
+    //   console.log(document.documentElement.style.fontSize);
+    //   document.documentElement.style.fontSize = document.documentElement.clientWidth / 100 + 'px';
+    // }
+    // document.documentElement.style.fontSize = document.documentElement.clientWidth / 100 + 'px';
   },[]);
   return (
     <div>
