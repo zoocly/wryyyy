95c5ff622a68b4f83d0f82dd9ca7c73099fad9a6
clymene <326795631@qq.com>
2021-04-07 11:16:50 +0800

    使用svg

--- src/pages/note/deep/index.tsx
+++ src/pages/note/deep/index.tsx
@@ -1,12 +1,13 @@
 import React, { Fragment } from 'react';
 import { Divider } from 'antd';
 import MapSet from './components/mapSet';
+import { ReactComponent as A } from '@/assets/test.svg';
 
 export default function index() {
   return (
     <Fragment>
+      <A/>
       <Divider />
-
       <a href="https://juejin.im/post/59ac1c4ef265da248e75892b" target={'_blank'}>js 深拷贝 vs 浅拷贝</a> <br />
       基本类型会指向不同的堆，引用类型会指向同一个堆的数据 <br />
       1. 浅拷贝只能拷贝基本类型，所以解构不能拷贝 {JSON.stringify([{ a: 1 }])} <br />
